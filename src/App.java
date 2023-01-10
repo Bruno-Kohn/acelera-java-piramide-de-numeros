09a6369b347678c66a4e44dd7a88f90c02e27c55
Bruno Kohn <brunokohn1993@gmail.com>
2023-01-09 22:49:09 -0300

    feat: piramide de numeros exercise

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    public static void main(String[] args) {
+      int myNumber = 7;
+  
+      for (int i = 1; i <= myNumber; i++) {
+        for (int j = 1; j <= i; j++) {
+          System.out.print(i);
+        }
+        System.out.println();
+      }
+    }
+  }
